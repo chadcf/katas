5e400d87d2f9ffb51a339fbe2f5ebd82b59b7abe
Chad Cunningham <ccunningham@innova-partners.com>
2013-05-07 22:35:31 -0700

    add mergesort implementation

---
+++ mergesort.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+#
+# mergesort.rb
+#
+# pure ruby merge sort implementation
+
+def msort(arr)
+  n = arr.length / 2
+  if (n == 0)
+    arr
+  else
+    merge msort(arr[0..n-1]), msort(arr[n..arr.length])
+  end
+end
+
+def merge(first, rest)
+  if first.empty?
+    rest
+  else
+    if rest.empty?
+      first
+    elsif first[0] < rest[0]
+      merge(first.drop(1), rest).unshift first[0]
+    else
+      merge(first, rest.drop(1)).unshift rest[0]
+    end
+  end
+end
+
+input = [1, 5, 8, 13, 4, 2, 7]
+output = msort(input)
+
+puts output.join(", ")
